4184f452663dd04f0b7ab15ee2feefaf821a4053
Pascal Lüttmann <pluettmann@uos.de>
2018-12-20 14:06:08 +0100

    PIT fertig

--- machine/pit.cc
+++ machine/pit.cc
@@ -11,5 +11,26 @@
 #include "machine/pit.h"
 
 void PIT::interval(int us) {
-    this -> interval = us;
+    this -> us = us;
+
+    double temp = (double)this -> us;
+
+    // intervall normieren
+    // 15/(4*PI) = 1,193 also sehr nah an 1,19366207 dran
+    temp *= 15;
+    temp /= (4*PI);
+
+    char ergebnis = (char)temp;
+
+    // Steuerregister wie folgt beschreiben
+    // 00, damit Zähler 0
+    // 11, niederwertiges, anschließend höherwertiges Byte
+    // 010, damit periodische Unterbrechungen
+    // 0, binäre Zählung von 16 Bit
+    // ergibt 0b00110100 = 0x34
+    this -> steuerregister.outb(0x34);
+
+    // niederwertiges Byte und dann höherwertiges Byte schreiben
+    this -> zaehler.outb(ergebnis);
+    this -> zaehler.outb(ergebnis >> 8);
 }
--- machine/pit.h
+++ machine/pit.h
@@ -11,11 +11,18 @@
 #ifndef __pit_include__
 #define __pit_include__
 
+#include "machine/io_port.h"
+
+#define PI 3.14159265358979323846  /* pi */
+
 class PIT {
 private:
     PIT(const PIT &copy); // Verhindere Kopieren
 
-    int interval;
+    IO_Port steuerregister;
+    IO_Port zaehler;
+
+    int us;
 
 public:
     /*
@@ -24,16 +31,14 @@ public:
      * Auflösung des Timerbausteins maximal 838 Nanosekunden beträgt, ist
      * keine ganz exakte Einstellung im Mikrosekundenbereich möglich.
      */
-    PIT (int us) {
+    PIT (int us) : steuerregister(0x43), zaehler(0x40){
       interval (us);
     }
 
     /*
      * Gibt an, welches Unterbrechungsintervall eingestellt wurde.
      */
-    int interval () {
-        return this -> interval;
-    }
+    int interval () {return this -> us;}
 
     /*
      * Stellt das Unterbrechungsintervall neu ein.
